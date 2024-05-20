01008abca5b8255917c6e0c416e7f322542b91e2
windows1223 <163809584+windows1223@users.noreply.github.com>
2024-05-20 14:39:26 +0900

    w12

---
+++ w12/master.js
@@ -0,0 +1,59 @@
+function nightDayHandler(){
+var target = document.querySelector('body');      
+if(self.value == 'day'){
+  target.style.backgroundColor = 'white';
+  target.style.color = 'black';
+  self.value = 'night';
+  document.querySelector('#night_day_2').value = 'night';
+} else {
+  target.style.backgroundColor = 'black';
+  target.style.color = 'white';
+  self.value = 'day';
+  document.querySelector('#night_day_2').value = 'day';
+}
+}
+
+function three(){
+  var target = document.querySelector('body');      
+        if(self.value == 'day'){
+          target.style.backgroundColor = 'white';
+          target.style.color = 'black';
+          self.value = 'night';
+          document.querySelector('#night_day_2').value = 'night';
+        } else {
+          target.style.backgroundColor = 'black';
+          target.style.color = 'white';
+          self.value = 'day';
+          document.querySelector('#night_day_2').value = 'day';
+        }
+}
+
+function two(){
+  var target = document.querySelector('body');      
+        if(self.value == 'day'){
+          target.style.backgroundColor = 'white';
+          target.style.color = 'black';
+          self.value = 'night';
+          document.querySelector('#night_day_2').value = 'night';
+        } else {
+          target.style.backgroundColor = 'black';
+          target.style.color = 'white';
+          self.value = 'day';
+          document.querySelector('#night_day_2').value = 'day';
+        }
+      }
+
+      function one(){
+        var target = document.querySelector('body');      
+        if(self.value == 'day'){
+          target.style.backgroundColor = 'white';
+          target.style.color = 'black';
+          self.value = 'night';
+          document.querySelector('#night_day_2').value = 'night';
+        } else {
+          target.style.backgroundColor = 'black';
+          target.style.color = 'white';
+          self.value = 'day';
+          document.querySelector('#night_day_2').value = 'day';
+        }
+      }
